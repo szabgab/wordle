5a3f095aacbd06fd1bbe0e1e109c281681670115
Gabor Szabo <gabor@szabgab.com>
2022-01-29 13:43:24 +0200

    check urls

---
+++ check.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def main():
+    error = 0
+    with open('README.md') as fh:
+        for row in fh:
+            match = re.search(r'\(([^\)]*)\)', row)
+            if match:
+                url = match.group(1)
+                print(url)
+                rv = requests.get(url)
+
+                # Return 406 for the request
+                if url == 'https://urdle.chaoticity.com/':
+                    continue
+
+                if rv.status_code == 200:
+                    dom = BeautifulSoup(rv.content,  'html.parser')
+                    print(dom.title)
+                else:
+                    print(f"ERROR: {url}  {rv.status_code}")
+                    error += 1
+                print()
+    exit(error)
+
+main()
+
